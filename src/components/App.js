27e52a72d52e3737c3f052d8d6e01f685c77a377
tsubasa1122 <nakanot@open8.com>
2019-05-04 14:46:10 +0900

    finish basic

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {increment, decrement} from '../actions';
 
-const App = () => {
-  return <Container />;
-};
-
-class Container extends Component {
+class App extends Component {
+  constructor (props) {
+    super (props);
+  }
   render () {
-    return <div>container</div>;
+    const props = this.props;
+    console.log (props);
+    return (
+      <React.Fragment>
+        <div>value:{props.value}</div>
+        <button onClick={props.increment}>+</button>
+        <button onClick={props.decrement}>-</button>
+      </React.Fragment>
+    );
   }
 }
-export default App;
+
+const mapStateToProps = state => ({
+  value: state.count.value,
+});
+
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch (increment ()),
+  decrement: () => dispatch (decrement ()),
+});
+export default connect (mapStateToProps, mapDispatchToProps) (App);
