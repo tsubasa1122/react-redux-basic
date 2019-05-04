da79e6234c516bad8dda459552d3c50f01af0f47
tsubasa1122 <nakanot@open8.com>
2019-05-04 11:27:27 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import {userInfo} from 'os';
 
-class App extends Component {
-  render () {
-    const greeting = 'Hi!';
-    return <React.Fragment>{greeting}</React.Fragment>;
-  }
-}
+const App = () => {
+  const profiles = [{name: 'taro', age: 10}, {name: 'hanako', age: 7}];
+  return (
+    <React.Fragment>
+      {profiles.map ((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
+    </React.Fragment>
+  );
+};
+
+const User = props => {
+  return <div>hi my name {props.name} and {props.age}</div>;
+};
 
 export default App;
