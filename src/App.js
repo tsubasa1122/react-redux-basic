dd99505753ea8dd333021206eaf7a51b1c1cf13b
tsubasa1122 <nakanot@open8.com>
2019-05-04 11:50:37 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 
 const App = () => {
-  const profiles = [
-    {name: 'taro', age: 10},
-    {name: 'hanako', age: 7},
-    {name: 'noname'},
-  ];
-  return (
-    <React.Fragment>
-      {profiles.map ((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />;
-      })}
-    </React.Fragment>
-  );
+  return <Container />;
 };
 
-const User = props => {
-  return <div>hi my name {props.name} and {props.age}</div>;
-};
-
-User.propTypes = {
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number,
-};
-
+class Container extends Component {
+  render () {
+    return <div>container</div>;
+  }
+}
 export default App;
